cd8ed0affa249bb92f700f339e8dbfc3d876d19c
Danilo Lazic <61419348+dnllzc@users.noreply.github.com>
2024-11-18 12:04:58 +0100

    Update agent.py
    Updated the IPs from local ips used for testing to the placeholder <MASTER_IP>

--- agent.py
+++ agent.py
@@ -6,7 +6,7 @@ def register_with_master():
     """Register the agent with the master server."""
     context = zmq.Context()
     reg_socket = context.socket(zmq.REQ)  # REQ to register
-    reg_socket.connect("tcp://192.168.56.10:9294")  # connect to master registration
+    reg_socket.connect("tcp://<MASTER_IP>:9294")  # connect to master registration
     agent_id = socket.gethostname()
     reg_socket.send_string(agent_id)
     ack = reg_socket.recv_string()
@@ -16,12 +16,12 @@ def main():
     context = zmq.Context()
     # SUB for receiving commands
     command_socket = context.socket(zmq.SUB)
-    command_socket.connect("tcp://192.168.56.10:9292")
+    command_socket.connect("tcp://<MASTER_IP>:9292")
     command_socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
     # PUSH for sending response
     response_socket = context.socket(zmq.PUSH)
-    response_socket.connect("tcp://192.168.56.10:9293")
+    response_socket.connect("tcp://<MASTER_IP>:9293")
 
     register_with_master()
 
